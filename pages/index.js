607357986215a3a2f36713dbdb926a7b8beba54a
Matthew Podolak <mpodola2@gmail.com>
2021-04-30 08:46:02 -0400

    added default date

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,24 @@ export default function Home({ clinics, lastUpdate }) {
 export async function getServerSideProps(ctx) {
   console.log(process.env.NODE_ENV);
   const res = await fetch(`${server}/api/clinics/info`);
+  const defaultDate = new Date();
+
+  var options = {
+    timeZone: 'America/New_York',
+    hour12: true,
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
 
   try {
-    const { clinics = 0, lastUpdate } = await res.json();
+    const {
+      clinics = 0,
+      lastUpdate = `${defaultDate.toLocaleString('en-US', options)}`,
+    } = await res.json();
     return {
       props: { clinics, lastUpdate }, // will be passed to the page component as props
     };
