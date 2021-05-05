b2c5644a0b4ec746d6f9384606eabf394a7c4d45
Matthew Podolak <mpodola2@gmail.com>
2021-05-05 11:14:07 -0400

    sign up for notification at any time

--- atomic/molecules/ResultsHeader.js
+++ atomic/molecules/ResultsHeader.js
@@ -1,7 +1,7 @@
 export const ResultsHeader = ({ length }) => {
   const header = length > 0 ? 'Search results' : 'No results found';
   return (
-    <div className="mb-2">
+    <div>
       <h3 className="text-2xl font-bold">{header}</h3>
     </div>
   );
