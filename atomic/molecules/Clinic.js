5740b468d633ffdef06569fdb42de3e2f0b92e58
Matthew Podolak <mpodola2@gmail.com>
2021-05-05 09:04:57 -0400

    render tweet info and optional register button

--- atomic/molecules/Clinic.js
+++ atomic/molecules/Clinic.js
@@ -3,24 +3,38 @@ import { LastUpdated } from '@/atoms/LastUpdated';
 import { Pill } from '@/molecules/Pill';
 
 export const Clinic = (props) => {
-  const { name, booking_link, eligibility, lastUpdated, source } = props;
+  const {
+    name,
+    booking_link,
+    eligibility,
+    lastUpdated,
+    source,
+    screen_name,
+  } = props;
   return (
     <div className="flex flex-col my-2 space-y-2 shadow-md p-5">
       <div className="flex flex-col items-left">
-        <h1 className="text-lg font-medium">{name}</h1>
+        <div className="flex space-x-1 items-center">
+          <h1 className="text-lg font-medium">{name}</h1>
+          {screen_name && (
+            <h2 className="text-gray-400 font-medium">@{screen_name}</h2>
+          )}
+        </div>
         <LastUpdated lastUpdate={lastUpdated} />
         <Pill text={source} infoText={'information source'} />
       </div>
       <div className="flex">
         <p>{eligibility}</p>
       </div>
-      <div className="flex">
-        <div className="self-end my-1">
-          <a target="_blank" href={booking_link}>
-            <Button>Register</Button>
-          </a>
+      {booking_link && (
+        <div className="flex">
+          <div className="self-end my-1">
+            <a target="_blank" href={booking_link}>
+              <Button>Register</Button>
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
